41e551bb710e2e6f17e4f81d448d6e49702a8b17
jejenfisika-stack <jejen.fisika@gmail.com>
2026-04-22 11:04:33 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['rose-casual-warbler-710.mypinata.cloud', 'gateway.pinata.cloud'],
+    domains: [
+      'rose-casual-warbler-710.mypinata.cloud',
+      'gateway.pinata.cloud',
+      'images.unsplash.com',
+    ],
   },
 }
 
